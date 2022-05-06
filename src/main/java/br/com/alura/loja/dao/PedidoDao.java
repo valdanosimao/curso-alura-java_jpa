09db3756829541ab81241560dc8320621865c617
Valdano Simão <valdanosimao@gmail.com>
2022-05-06 17:38:35 -0300

    implementando as anotações Lazer e Eager para performance de contultas, e join fetch

--- src/main/java/br/com/alura/loja/dao/PedidoDao.java
+++ src/main/java/br/com/alura/loja/dao/PedidoDao.java
@@ -20,63 +20,21 @@ public class PedidoDao {
 		this.em.persist(pedido);
 	}
 
-	public void atualizar(Pedido pedido) {
-		this.em.merge(pedido);
+	public BigDecimal valorTotalVendido() {
+		String jpql = "SELECT SUM(p.valorTotal) FROM Pedido p";
+		return em.createQuery(jpql, BigDecimal.class).getSingleResult();
 	}
 
-	public void remover(Pedido pedido) {
-		pedido = em.merge(pedido);
-		this.em.remove(pedido);
+	public List<RelatorioDeVendasVo> relatorioDeVendas() {
+		String jpql = "SELECT new br.com.alura.loja.vo.RelatorioDeVendasVo(" + "produto.nome, "
+				+ "SUM(item.quantidade), " + "MAX(pedido.data)) " + "FROM Pedido pedido " + "JOIN pedido.itens item "
+				+ "JOIN item.produto produto " + "GROUP BY produto.nome " + "ORDER BY item.quantidade DESC";
+		return em.createQuery(jpql, RelatorioDeVendasVo.class).getResultList();
 	}
-	
-	public Pedido buscarPorId(Long id) {
-		return em.find(Pedido.class, id);
-	}
-	
-	public List<Pedido> buscarTodos() {
-		String jpql = "SELECT p FROM Pedido p";
-		return em.createQuery(jpql, Pedido.class).getResultList();
-	}
-	
-	public List<Pedido> buscarPorNome(String nome) {
-		String jpql = "SELECT p FROM Pedido p WHERE p.nome = :nome";
-		return em.createQuery(jpql, Pedido.class)
-				.setParameter("nome", nome)
-				.getResultList();
-	}
-	
-	public List<Pedido> buscarPorNomeDaCategoria(String nome) {
-		String jpql = "SELECT p FROM Pedido p WHERE p.categoria.nome = :nome";
-		return em.createQuery(jpql, Pedido.class)
-				.setParameter("nome", nome)
-				.getResultList();
-	}
-	
-	public BigDecimal buscarPrecoDoProdutoComNome(String nome) {
-		String jpql = "SELECT p.preco FROM Pedido p WHERE p.nome = :nome";
-		return em.createQuery(jpql, BigDecimal.class)
-				.setParameter("nome", nome)
-				.getSingleResult();
-	}
-	
-	public BigDecimal valotTotalVendido() {
-		String jpql = "SELECT SUM(p.valorTotal) FROM Pedido p";
-		return em.createQuery(jpql, BigDecimal.class)
-				.getSingleResult();
-	}
-	
-	public List<RelatorioDeVendasVo> relatorioDeVendas(){
-		String jpql = "SELECT new br.com.alura.loja.vo.RelatorioDeVendasVo( "
-						+ "produto.nome," 
-						+ "SUM(item.quantidade),"
-						+ "MAX(pedido.data))"
-						+ "FROM Pedido pedido"
-						+ "JOIN pedido.itens item"
-						+ "JOIN item.produto produto"
-						+ "GROUP BY produto.nome"
-						+ "ORDER BY item.quantidade DESC";
-		return em.createQuery(jpql, RelatorioDeVendasVo.class)
-				.getResultList();
+
+	public Pedido buscarPedidoComCliente(Long id) {
+		return em.createQuery("SELECT p FROM Pedido p JOIN FETCH p.cliente WHERE p.id = :id", Pedido.class)
+				.setParameter("id", id).getSingleResult();
 	}
 
 }
